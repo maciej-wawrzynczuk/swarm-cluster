64f59d4a48ae62c9902560693f2cc4bd4462fec9
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2019-07-31 12:00:56 +0200

    ingress secured

--- main.tf
+++ main.tf
@@ -21,6 +21,10 @@ data "aws_security_group" "default" {
   name = "default"
 }
 
+data "aws_vpc" "my_default_vpc" {
+  default = true
+}
+
 #TODO: restrict cidr
 resource "aws_security_group" "swarm-node" {
   name = "swarm-node"
@@ -30,35 +34,35 @@ resource "aws_security_group" "swarm-node" {
     from_port = 2376
     to_port = 2376
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.aws_vpc.my_default_vpc.cidr_block}"]
   }
 
   ingress {
     from_port = 2377
     to_port = 2377
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.aws_vpc.my_default_vpc.cidr_block}"]
   }
 
   ingress {
     from_port = 7946
     to_port = 7946
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.aws_vpc.my_default_vpc.cidr_block}"]
   }
 
   ingress {
     from_port = 7946
     to_port = 7946
     protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.aws_vpc.my_default_vpc.cidr_block}"]
   }
 
    ingress {
     from_port = 4789
     to_port = 4789
     protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${data.aws_vpc.my_default_vpc.cidr_block}"]
    }
 
    egress {
